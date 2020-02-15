92d6437c4258584f43949fd6d9bbb94d5a38c1f4
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2020-02-15 12:32:22 -0800

    ESP8266 Basic Tests
    Created HTTP Get Test, HTTP Post Test (Adafruit.io), Ultrasonic Test, and Servo Test.

---
+++ Firmware/ESP8266_Ultrasonic_Test/ESP8266_Ultrasonic_Test.ino
@@ -0,0 +1,55 @@
+const int TRIG_PIN = 4;
+const int ECHO_PIN = 5;
+
+// Anything over 400 cm (23200 us pulse) is "out of range"
+const unsigned int MAX_DIST = 23200;
+
+void setup() {
+
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+  pinMode(ECHO_PIN, INPUT);
+
+  Serial.begin(115200);
+}
+
+void loop() {
+
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  float cm;
+  float inches;
+
+  // Hold the trigger pin high for at least 10 us
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  while (digitalRead(ECHO_PIN) == 0); // wait till we get a response
+
+  // Measure how long the echo pin was held high (pulse width)
+  // Note: the micros() counter will overflow after ~70 min
+  t1 = micros();
+  while (digitalRead(ECHO_PIN) == 1);
+  t2 = micros();
+  pulse_width = t2 - t1;
+
+  // Calculate distance in centimeters and inches. The constants
+  // are found in the datasheet, and calculated from the assumed speed
+  // of sound in air at sea level (~340 m/s).
+  cm = pulse_width / 58.0;
+  inches = pulse_width / 148.0;
+
+  // Print out results
+  if ( pulse_width > MAX_DIST ) {
+    Serial.println("Out of range");
+  } else {
+    Serial.print(cm);
+    Serial.print(" cm \t");
+    Serial.print(inches);
+    Serial.println(" in");
+  }
+
+  delay(60);
+}
