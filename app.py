515f40a317c1b871ed942ff959375eec2c97ca0b
Colin Schultz <colinschultz@Colins-MacBook-Pro-2.local>
2020-02-15 22:39:29 -0800

    no db

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import traceback
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trashcan.db'
-db = SQLAlchemy(app)
-db.create_all()
 
-class trashcan(db.Model):
-	dustbin_id = db.Column(db.Integer, primary_key = True, unique = True)
-	trash = db.Column(db.Integer, nullable = True)
-	compost = db.Column(db.Integer, nullable = True)
-	recycling = db.Column(db.Integer, nullable = True)
-	def __repr__(self):
-		return str(self.__dict__)
+class Trashcan():
+	def __init__(self, id):
+		self.id = id
+		self.trash = 0
+		self.recycling = 0
+		self.compost = 0
 
+cans = {}
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
 	
 	if request.method == 'GET':
-		bins = trashcan.query.all()
-		return render_template('index.html', bins = bins)
+		return render_template('index.html', bins = cans)
 
 	elif request.method == "POST": 
 		try:
@@ -31,28 +26,13 @@ def index():
 			argument_compost = request.form["compost"]
 			argument_trash = request.form["trash"]
 
-			if trashcan.query.filter_by(dustbin_id = dustbin_id).first() is None:
-				new_dustbin = trashcan(dustbin_id = dustbin_id, trash = argument_trash, compost = argument_compost, recycling = argument_recycling)
-				db.session.add(new_dustbin)
-				# try :
-				# db.session.commit()
-				return "New record added"
-				# except e:
-				# 	return str(e)
+			if id not in cans:
+				cans[id] = Trashcan(id)
+			
+			cans[id].trash = argument_trash
+			cans[id].recycling = argument_recycling
+			cans[id].compost = argument_compost
 
-			else:
-				target_dustbin = trashcan.query.get(dustbin_id)
-				
-				target_dustbin.trash = argument_trash
-				target_dustbin.compost = argument_compost
-				target_dustbin.recycling = argument_recycling
-				
-				#try:
-				# db.session.commit()
-				return "200\n"
-
-				# except e:
-				# 	return str(e)
 		except Exception as e:
 			traceback.print_exc()
 			return str(e)
