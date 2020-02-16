5468d79af55dcac887e2e4bcf3f64d7f580071fb
Mehul <aroram8@stanford.edu>
2020-02-15 20:25:07 -0800

    BACKEND IS DONE

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trashcan.db'
 db = SQLAlchemy(app)
-
+db.create_all()
 
 class trashcan(db.Model):
 	dustbin_id = db.Column(db.Integer, primary_key = True, unique = True)
@@ -19,36 +19,35 @@ class trashcan(db.Model):
 @app.route('/', methods = ['POST', 'GET'])
 def index():
 	
-	if request.method == 'POST':
-		dustbin_id = request.args.get('id')
-
-		if target_dustbin is None:
-			return render_template('index.html', bins = bins)
-		
-		argument_recycling = request.args.get('recycling')
-		argument_compost = request.args.get('compost')
-		argument_trash = request.args.get('trash')
-
-		target_dustbin = trashcan.query.get(dustbin_id)
-
-		
-
-		target_dustbin.trash = argument_trash
-		target_dustbin.compost = argument_compost
-		target_dustbin.recycling = argument_recycling
-		
-		try:
-			db.session.commit()
-			bins = trashcan.query.all()
-			return render_template('index.html', bins = bins)
-
-		except:
-			return "Internal Failure"
-
-		
-	else: 
+	if request.method == 'GET':
 		bins = trashcan.query.all()
 		return render_template('index.html', bins = bins)
+
+	elif request.method == "POST": 
+		dustbin_id = request.form['id']
+		argument_recycling = request.form["recycling"]
+		argument_compost = request.form["compost"]
+		argument_trash = request.form["trash"]
+
+		if trashcan.query.filter_by(dustbin_id = dustbin_id).first() is None:
+			new_dustbin = trashcan(dustbin_id = dustbin_id, trash = argument_trash, compost = argument_compost, recycling = argument_recycling)
+			db.session.add(new_dustbin)
+			db.session.commit()
+			return "New record added"
+
+		else:
+			target_dustbin = trashcan.query.get(dustbin_id)
+			
+			target_dustbin.trash = argument_trash
+			target_dustbin.compost = argument_compost
+			target_dustbin.recycling = argument_recycling
+			
+			try:
+				db.session.commit()
+				return "200\n"
+
+			except:
+				return "Internal Failure"
 
 
 if __name__ == "__main__":
